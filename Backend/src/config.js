f4c0df4bde1a1824fa8d4f016c310d8b4dee6dac
PC2001 <pedrocastrocal@gmail.com>
2025-04-27 18:23:26 -0600

    fix: edit config.js

--- Backend/src/config.js
+++ Backend/src/config.js
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "Pedro";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "artdeco238";
-export const DB_DATABASE = process.env.DB_DATABASE || "inventario_lab_bs";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const PORT = process.env.PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
