ad0b3d8f5ad87c20224d0c08d67c8132c2aa1ff0
PC2001 <pedrocastrocal@gmail.com>
2025-11-16 19:36:59 -0600

    Se hicieron cambios en las referencias de variables de entorno

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,13 +17,15 @@ import session from "express-session";
 const app = express();
 
 // Middlewares
-app.use(morgan("dev"));
+
+app.use(cors());
+/*app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: false,
-}));
+}));*/
 
 // Routes
 app.use("/", indexRoutes);
